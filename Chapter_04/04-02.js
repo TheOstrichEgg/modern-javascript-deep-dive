f990df0d0987c1afed7d037563f8b7d3fa4411c8
YongJoon Kim <ostricheggyj@gmail.com>
2022-05-10 22:35:27 +0900

    Resolve #2 Chapter 04

---
+++ Chapter_04/04-02.js
@@ -0,0 +1,9 @@
+var userId = 1;
+var userName = 'YJ'
+
+var user = { id: 1, name: 'YJ'};
+
+var users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' }
+];
---
+++ Chapter_04/04-08.js
@@ -0,0 +1,6 @@
+console.log(score);
+
+var score; // declaration
+score = 80; // assignment
+
+console.log(score);
