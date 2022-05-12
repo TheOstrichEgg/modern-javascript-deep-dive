282af95f55897b54c17d180875cfd2f2949f8f3f
YongJoon Kim <ostricheggyj@gmail.com>
2022-05-12 23:15:02 +0900

    Resolve #3 Chapter 05

---
+++ Chapter_05/05-09.js
@@ -0,0 +1,14 @@
+// variable declaration
+var x;
+
+// assignment statment
+x = 5;
+
+// function declaration
+function foo() {}
+
+// conditional
+if (x > 1) { console.log(x); }
+
+// iteration
+for (var i = 0; i < 2; i++) { console.log(i); }
