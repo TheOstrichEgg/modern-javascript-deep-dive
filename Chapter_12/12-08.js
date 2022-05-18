5b3bfe40db97dfecae27286cba2c33d769fccfc5
YongJoon Kim <ostricheggyj@gmail.com>
2022-05-18 23:47:03 +0900

    Ongoing #10 Chapter 12
    Function

---
+++ Chapter_12/12-08.js
@@ -0,0 +1,9 @@
+function foo() {
+    console.log('foo');
+}
+foo();
+
+(function bar() {
+    console.log('bar');
+});
+bar();
---
+++ Chapter_12/12-10.js
@@ -0,0 +1,6 @@
+// function expression
+var add = function(x, y) {
+    return x + y
+};
+
+console.log(add(2, 5));
